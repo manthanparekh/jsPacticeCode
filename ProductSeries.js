71799751e53647cf48ffd85b7bea00b321e9a347
manthanparekh <manthan.parekh@outlook.com>
2022-06-25 21:04:30 -0400

    Product/Pi Series

---
+++ ProductSeries.js
@@ -0,0 +1,23 @@
+// Product Series / Pi notation
+// Multipling from 1 to end amount 
+// 1 * 2 * 3 * ... * n = n!   
+/*
+0! = 1
+1! = 1
+2! = 2 * 1 = 2
+*/
+
+function factorial(n) {
+    if (n == 0) {
+        return 1; 
+    } else {
+        return n * factorial(n-1);
+    }
+}
+
+console.log(facotrial(0));
+console.log(factorial(1)); // = 1
+console.log(factorial(2)); // = 2
+console.log(factorial(3)); // = 6
+console.log(factorial(4)); // = 24
+console.log(factorial(5)); // = 120 
